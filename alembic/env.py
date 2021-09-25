48de8473c55a639ea0177f97f11329686e0c1a8e
rcastellotti <me@rcastellotti.dev>
2021-09-26 01:30:37 +0200

    no postdeploy

--- alembic/env.py
+++ alembic/env.py
@@ -24,11 +24,6 @@ fileConfig(config.config_file_name)
 # from myapp import mymodel
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
-
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
